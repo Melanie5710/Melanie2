8fd011283310db315c62bcd157a44c9862e5e98e
Melanie5710 <melma2459@gmail.com>
2021-08-11 20:21:43 -0500

    Added 2 classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorldFTC.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+
+public class HelloWorldFTC extends OpMode {
+    private DcMotorController dcMotorController;
+
+    private DcMotor dcDriverLeft;
+    private DcMotor dcDriverRight;
+
+    @Override
+    public void init() {
+        dcMotorController = hardwareMap.dcMotorController.get("drive_controller");
+        dcDriverLeft = hardwareMap.dcMotor.get("drive_left");
+        dcDriverRight = hardwareMap.dcMotor.get("drive_right");
+    }
+
+    @Override
+    public void loop() {
+        dcDriverLeft.setPower(gamepad1.left_stick_y);
+        dcDriverRight.setPower(gamepad1.right_stick_y);
+    }
+}
